6a74675600844b5bff3bbeb35c900098137f71be
muresanrazvan <razvan.muresan@nokia.com>
2018-09-03 18:13:46 +0300

    minor improvements

--- app/src/main/java/com/environment/licenta/environmentmonitor/utils/NotificationService.java
+++ app/src/main/java/com/environment/licenta/environmentmonitor/utils/NotificationService.java
@@ -1,13 +1,16 @@
 package com.environment.licenta.environmentmonitor.utils;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
 
 import com.environment.licenta.environmentmonitor.model.ProgramData;
+import com.environment.licenta.environmentmonitor.model.ServiceData;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -24,6 +27,27 @@ public class NotificationService extends Service {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("users");
         myRef.addValueEventListener(new NotificationValueEventListener(this));
+
+        ServiceData serviceData=ServiceData.getInstance();
+        SharedPreferences prefs = this.getSharedPreferences("com.environment.licenta.environmentmonitor", Context.MODE_PRIVATE);
+
+        serviceData.minTemperature=prefs.getFloat("environment.minTemperature", Context.MODE_PRIVATE);
+        serviceData.minHumidity=prefs.getFloat("environment.minHumidity", Context.MODE_PRIVATE);
+        serviceData.minLight=prefs.getFloat("environment.minLight", Context.MODE_PRIVATE);
+        serviceData.minNoise=prefs.getFloat("environment.minNoise", Context.MODE_PRIVATE);
+        serviceData.minCO2=prefs.getFloat("environment.minCO2", Context.MODE_PRIVATE);
+
+        serviceData.maxTemperature=prefs.getFloat("environment.maxTemperature", Context.MODE_PRIVATE);
+        serviceData.maxHumidity=prefs.getFloat("environment.maxHumidity", Context.MODE_PRIVATE);
+        serviceData.maxLight=prefs.getFloat("environment.maxLight", Context.MODE_PRIVATE);
+        serviceData.maxNoise=prefs.getFloat("environment.maxNoise", Context.MODE_PRIVATE);
+        serviceData.maxCO2=prefs.getFloat("environment.maxCO2", Context.MODE_PRIVATE);
+
+        serviceData.temperatureEnabled=prefs.getBoolean("environment.temperatureAlert",false);
+        serviceData.humidityEnabled=prefs.getBoolean("environment.humidityAlert",false);
+        serviceData.lightEnabled=prefs.getBoolean("environment.lightAlert",false);
+        serviceData.noiseEnabled=prefs.getBoolean("environment.noiseAlert",false);
+        serviceData.eCO2Enabled=prefs.getBoolean("environment.CO2Alert",false);
     }
 
     @Override
