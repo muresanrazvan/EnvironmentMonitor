a61c0b0796c75a6bf6c3e780ec03aedb7828bf14
muresanrazvan <andrei.mure96@gmail.com>
2019-02-09 21:15:01 +0200

    added connection loss handling

--- arduino_data_collector.py
+++ arduino_data_collector.py
@@ -2,6 +2,7 @@
 
 import serial, string, datetime,time, re
 import pyrebase
+import urllib2
 
 config = {
   "apiKey": "AIzaSyCSdmaI7jErihMER3VJtv4WMQ44mP0nEgE",
@@ -13,60 +14,82 @@ config = {
 firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 
-data = {
-}
 
+waiting_data=[]
+
+def connection_check():
+    try:
+        urllib2.urlopen("https://www.google.com/")
+        return True
+    except:
+        return False
+
+def send_to_firebase(data,last_datetime):
+    if connection_check():
+        if len(waiting_data)>0:
+            while(len(waiting_data)>0):
+                tmp_data=waiting_data.pop(0)
+                db.child("users").child(tmp_data["last_datetime"].replace(":","-").replace(" ","-").replace(".","-")).set(tmp_data["data"])
+                print("uploading waiting data"+str(tmp_data["data"]))
+                
+        db.child("users").child(last_datetime.replace(":","-").replace(" ","-").replace(".","-")).set(data)
+    else:
+        print("failed to upload")
+        waiting_data.append({"data":data,"last_datetime":last_datetime})
 
 output = " "
 ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
 
 count=0
 while True:
+  data={}
   print("----")
+  last_datetime=str(datetime.datetime.now())
+  timestamp=int(round(time.time() * 1000))
   if count==0:
       file=open("output.txt","a")
   while output != "":
     output = ser.readline()
     if "=" in output:
-        file.write(str(datetime.datetime.now())+'\n')
-        continue
+        file.write('\n')
+        file.write(last_datetime+'\n')
+        file.write("timestamp="+str(timestamp)+'\n')
     if "*C" in output:
         tmp=re.findall("\d+\.\d+", output)
         data["temperature"]=tmp[0]
         print ("temp",tmp[0])
-        file.write(tmp[0]+'\n')
-        continue
+        file.write("tmp="+tmp[0]+'\n')
     if "%" in output:
         hmd=re.findall("\d+\.\d+", output)
         print ("hum",hmd[0])
         data["humidity"]=hmd[0]
-        file.write(hmd[0]+'\n')
-        continue
+        file.write("hmd="+hmd[0]+'\n')
     if "Lux" in output:
         lux=re.findall("\d+\.\d+", output)
         print ("lux",lux[0])
         data["light"]=lux[0]
-        file.write(lux[0]+'\n')
+        file.write("lux="+lux[0]+'\n')
     if "dB" in output:
         dBs=re.findall("\d+\.\d+", output)
         print ("dBs",dBs[0])
         data["noise"]=dBs[0]
-        file.write(dBs[0]+'\n')
+        file.write("dbs="+dBs[0]+'\n')
     if "ppb" in output:
         tvoc=re.findall("\d+\.\d+", output)
         print ("ppb",tvoc[0])
         data["TVOC"]=tvoc[0]
-        file.write(tvoc[0]+'\n')
+        file.write("tvoc="+tvoc[0]+'\n')
     if "ppm" in output:
         eco2=re.findall("\d+\.\d+", output)
         print ("ppm",eco2[0])
         data["eCO2"]=eco2[0]
-        file.write(eco2[0]+'\n')
-        data["timestamp"]=int(round(time.time() * 1000))
-        db.child("users").child(str(datetime.datetime.now()).replace(":","-").replace(" ","-").replace(".","-")).set(data)
+        file.write("eco2="+eco2[0]+'\n')
+        data["timestamp"]=timestamp
+        send_to_firebase(data,last_datetime)
         continue
-    count+=1
+  count+=1
   if count>=50:
+      print("flushed to file")
       file.close()
       count=0
   output = " "
